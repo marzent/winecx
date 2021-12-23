fb1b460fee71718d3a7e51e0e3c94e0697b2b6aa
marzent <marc_aurel@me.com>
2021-12-23 09:33:38 +0100

    adding customized WINEDEBUG message

---
+++ programs/winedbg/distversion.h
@@ -0,0 +1,12 @@
+/* ---------------------------------------------------------------
+ *   distversion.c
+ *
+ * Copyright 2013, CodeWeavers, Inc.
+ * 
+ * Information from DISTVERSION which needs to find 
+ * its way into the wine tree.
+ * --------------------------------------------------------------- */
+
+#define WINDEBUG_WHAT_HAPPENED_MESSAGE "This can be caused by a problem in the program or a deficiency in Wine. You may want to check out our <a href=\"https://discord.gg/dWN5bTC4Yv\">discord</a> for tips about running this application."
+
+#define WINDEBUG_USER_SUGGESTION_MESSAGE "If this problem is not present under CrossOver or the native launcher and has not been reported yet, you can save the detailed information to a file using the \"Save As\" button, then <a href=\"https://github.com/marzent/winecx/issues\">file a bug report</a> and attach that file to the report."
