81f025ca0617fd8b337aedfff5978c0f1a57b9fa
Marc-Aurel Zent <marc_aurel@me.com>
2023-03-07 08:09:30 +0100

    esync: Use usleep(0) instead of NtYieldExecution()

--- dlls/ntdll/unix/esync.c
+++ dlls/ntdll/unix/esync.c
@@ -736,7 +736,7 @@ NTSTATUS esync_pulse_event( HANDLE handle )
 
     /* Try to give other threads a chance to wake up. Hopefully erring on this
      * side is the better thing to do... */
-    NtYieldExecution();
+    usleep(0);
 
     efd_read( obj );
 
