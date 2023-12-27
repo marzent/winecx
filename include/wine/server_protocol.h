db8154a2e9c6dfda14184e74ac1a7b8b90908b4d
Marc-Aurel Zent <mzent@codeweavers.com>
2023-12-27 19:10:38 +0100

    server: Remove foreground window from get_thread_input.

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -3898,12 +3898,10 @@ struct get_thread_input_reply
     user_handle_t  focus;
     user_handle_t  capture;
     user_handle_t  active;
-    user_handle_t  foreground;
     user_handle_t  menu_owner;
     user_handle_t  move_size;
     user_handle_t  caret;
     rectangle_t    rect;
-    char __pad_52[4];
 };
 
 
@@ -6615,7 +6613,7 @@ union generic_reply
 
 /* ### protocol_version begin ### */
 
-#define SERVER_PROTOCOL_VERSION 778
+#define SERVER_PROTOCOL_VERSION 779
 
 /* ### protocol_version end ### */
 
