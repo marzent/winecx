43d6447e7d702c702dc9aecdbaad726ccfe1ba7f
Marc-Aurel Zent <mzent@codeweavers.com>
2026-09-07 13:15:41 +0200

    win32u: Default EmulateModeset to TRUE.

--- dlls/win32u/sysparams.c
+++ dlls/win32u/sysparams.c
@@ -165,7 +165,7 @@ static INT64 last_query_display_time;
 static UINT64 monitor_update_serial;
 static pthread_mutex_t display_lock = PTHREAD_MUTEX_INITIALIZER;
 
-BOOL emulate_modeset = FALSE;
+BOOL emulate_modeset = TRUE;
 BOOL decorated_mode = TRUE;
 UINT64 thunk_lock_callback = 0;
 
